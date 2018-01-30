1cff3f1d7b2c46d3a327d59ae3e488ff36f513ac
lm-w1n5t0n <infinitemonkeys@tuta.io>
2018-01-30 16:15:57 +0000

    added handle

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,16 +4,20 @@ module Lib
 
 --import Control.Concurrent
 import Sound.File.Sndfile as SF
+import System.Directory
+import System.IO as IO
 
 
 someFunc :: IO ()
 someFunc = do
   let frames = 512
-      path = "home/Code/bob.w64"
+      fileName = "testFile.w64"
       info = Info frames 441000 1 format 1 True
       format = Format HeaderFormatW64 SampleFormatDouble EndianFile
       --format <- Format HeaderFormatW64 SampleFormatDouble EndianFile
-  hout <- SF.openFile path SF.WriteMode info
+      
+  IO.openFile fileName IO.ReadWriteMode -- write empty file
+  hout <- SF.openFile fileName SF.WriteMode info -- open a write handle
   print $ SF.duration info
-  SF.hClose hout
+  SF.hClose hout -- close handle
   return ()
