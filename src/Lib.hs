e79ce334075aade95bada272be70737bb1846e0c
lm-w1n5t0n <infinitemonkeys@tuta.io>
2018-01-30 15:00:38 +0000

    added hslibfile and some simple test code

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,18 @@ module Lib
     ( someFunc
     ) where
 
+--import Control.Concurrent
+import Sound.File.Sndfile as SF
+
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+  let frames = 512
+      path = "home/Code/bob.w64"
+      info = Info frames 441000 1 format 1 True
+      format = Format HeaderFormatW64 SampleFormatDouble EndianFile
+      --format <- Format HeaderFormatW64 SampleFormatDouble EndianFile
+  hout <- SF.openFile path SF.WriteMode info
+  print $ SF.duration info
+  SF.hClose hout
+  return ()
