0bf696be2694c5e2fe70ba0c8cfe21357c7a283b
lm-w1n5t0n <infinitemonkeys@tuta.io>
2018-03-13 20:43:18 +0000

    minor tweaks

--- Example.hs
+++ Example.hs
@@ -2,23 +2,25 @@ import Context
 import Util
 import Data.Fixed
 
+import TimeLines
+
 --Sets the current working window to be the first 10 seconds of the piece
 
 p1 = 0
 p2 = 5
 window p1 p2
 
-s "amp" $ \t -> scaleB (sin (2*pi*t * 3)) 0.01 0.4
-                
 
+bass = synth FM $ do 
+  s "amp" $ \t -> scaleB (sin (2*pi*t * 3)) 0.01 0.4
   --  
-s "freq" $ \t -> 150
+  s "freq" $ \t -> 150 + 150*2*sin $ 2*pi*t *20
   
   --  
-s "index" $ \t -> 150 * sin $ 2*pi * t
+  s "index" $ \t -> 1 * sin $ 2*pi * t
   
   --  
-s "ratio" $ \t -> 10 + 100 * rand $ 0.0000000002*t
+  s "ratio" $ \t -> 10 + 100 * rand $ 0.0000000002*t
 
 
 
