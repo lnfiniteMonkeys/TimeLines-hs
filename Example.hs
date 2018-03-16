530f3861b05dd0dbf1a02399de5ccb75b8c6e276
lm-w1n5t0n <infinitemonkeys@tuta.io>
2018-03-16 13:02:17 +0000

    added more comprehensive example with comments for further improvement

--- Example.hs
+++ Example.hs
@@ -1,62 +1,99 @@
+import Context
 
-
-
-{-
-bass = do
-  synth "fm" $ \t ->
-    let somestuff = 3
-        someOtherstuff = 10
-        foo = 3.14
-      
-    "amp"   <><  sin t
-               + cos t
-               + 0.5 * sin $ 2*pi*t
-               + onAt 3.5 15.0 t $ 100 * sin $ t*20
-    "freq"  <><  150
-    "ratio" <><  2.5
-               + onAt 0.2 5.0 t $ sin $ t*25 + t^8*25
-  
-  
--}
-
-
-
-
-
-
-  
-
---The below code works just fine, but I want to see how far I can push the syntax
-
+--Below is what currently works
 
 
 p1 = 0
 p2 = 5
---window 3.0 5.0
+--the window function modifies a global IORef that's used to determine
+--the time interval over which to evaluate all timelines
+--(ideally it should keep track of which synths/parameters are being controlled
+--and update them when the window changes, so that it acts more like a global transport)
+--will also be bound to keyboard shortcut
+window p1 p2
 
+--Example common workflow functions
 step t s = if (t < s) then 0 else 1
+--Simple AD envelope driven by an input t in seconds, increasing from 0
+env t atk rel e1 e2
+  | t > atk + rel = 0
+  | t < atk = (t/atk)**c1
+  | otherwise = (1 - (t-atk)/rel)**c2
+
+
+--ideally this would be used for higher-level realtime 
+let interval = p2-p1
+    numNotes = 8
+    noteDur = interval/numNotes
+    phasor t = wrap01 $ numNotes*(t/interval)
+    semitones = [0, 3, 3, 7, 5, 9, 7, 1]
+    fund = 200
+    ind t = fromIntegral $ (floor $ numNotes*t/interval)%(numNotes)
+    note t = fund * semi $ semitones!!ind (t*2)
+
+
+-- evaluates the functions over the current window (set above), writes
+-- them to files, and updates the buffers and synth
+"amp" <>< \t -> 0.8 * env (noteDur * phasor t) 0.005 (noteDur*0.5) 1.0 2.0
+--  
+"freq" <>< \t -> note t + lerp 0 1 (t/interval)**1 * 100 * (sin $ 2*pi*t *8)
+--  
+"index" <>< \t -> 100 + 400 * env (noteDur * phasor t) 0.0 (noteDur*0.9) 1.0 $ lerp 10.0 200.0 (t/interval)
+--  
+"ratio" <>< \t -> 10
+
+--(<><) :: String -> (Time -> Value) -> IO()
 
 {-
-let d = 4 -- duration in seconds
-    n = 2*4 -- number of notes (3 per beat, 4 beats per loop)  
-    d' = d/2 -- duration of each   
-
-
-
-
-"amp" <>< \t -> lfo t + env
-  --  
-"freq" <>< \t -> melody (wrap01 (t*5))
-  --  
-"index" <>< \t -> lerp 50 105 (t/8)**1 * (20 * (rand (t*0.1)))
-  --  
-"ratio" <>< \t -> 1 + 50 * (rand $ wrap01 (t*0.00000002))
-
-
+Problems with the above:
+-no way to specify which synthdef to use (hardcoded for now),
+-no way to work with/refer to multiple synths or group the parameters together
+-having to use a lambda for every parameter individually means I can't have a derivation
+from t that's available to all parameters of a synth (e.g. t' = wrap01 $ t*10)
 -}
 
---synth :: (Time -> IO()) -> Time -> IO()
 
 
+-- This is an example of how I think it can be improved (random code, doesn't run)
+bass = synth "fm" $ \t -> --one t for all parameters in a synth (ideally multiple synths too)
+    let somestuff = 3
+        someOtherstuff = 10
+        t' = t + 10 * sin $ 2*pi*t *10
+     
+    "amp"   <><  switch p1 p2 t * sin t
+               + switch p2 (p2+10) t * cos t
+               + 0.5 * sin $ 2*pi*t
+               + onAt 3.5 15.0 t $ 100 * sin $ t*20
+--being able to stack vertically like above would be great, allows to conceptually separate parts
+--that don't affect the parameters at overlapping time intervals and to avoid parens
+    "freq"  <><  let fund = 500
+                     semitones = indexList [0, 1, 2, 3, 4, 5, 6, 7] t
+                 in  fund*semitones
+--the let..in approach helps organize the mess and
+--keep the actual equations high level
+    "ratio" <><  let fund = 200
+                     mult = indexList [10, 20, 20, 35, 100, 50] t
+                 in  10 + fund*mult
+                   + onAt 0.2 5.0 t $ sin $ t*25 + t^8*25
+                   + 0.2 * sin $ t*0.5
+  
+  
+{-
+Notes:
+-ideally, the bulk of functions/composition would be happen at the top of the synth,
+so as to keep the actual parameter equations as simple as possible
 
+-"synth" should take a synthdef name, and a function of time that has multiple
+parameter components (something like synth :: SynthDef -> [Signal] -> IO() ?).
+
+- the synth name (in this case "bass") should be used to refer to the nodeID of the synth running
+and added to a global list of "working synths", so that when the global Window changes the synth
+would be updated by simply calling "bass" (which has type IO())in the interpreter, which would re-render
+the buffers using the new window
+
+-the function that sends the OSC (either "bass" or the individual (<><)s) should be aware of the synth name
+so that SC knows which nodeID to apply the amp, freq etc arguments to. Is this the writer monad?
+
+-indentation is a mess
+-}
 
