e2decf2eb79397e00837f8b3d32e155045c65635
lm-w1n5t0n <infinitemonkeys@tuta.io>
2018-06-18 18:35:06 +0100

    some cleanup and restructuring, added some instrument functions

--- Sound/TimeLines/TimeLines.hs
+++ Sound/TimeLines/TimeLines.hs
@@ -1,24 +1,13 @@
-
 module Sound.TimeLines.TimeLines where
 
---import Control.Concurrent
 import qualified Sound.File.Sndfile as SF
-import System.IO as IO
-import Foreign.Marshal.Array as MA
-import Foreign.Ptr
-import Foreign.ForeignPtr as FP
-import System.Directory as DIR
-import System.IO.Unsafe (unsafePerformIO)
-import System.IO
 
+--import System.IO as IO
+import Data.IORef (readIORef)
 
-import qualified Sound.OSC as OSC
-import qualified Sound.OSC.FD as FD
-
-import Sound.TimeLines.Signal
+import Sound.TimeLines.Types
 import Sound.TimeLines.Util
-
-import Prelude as Pr
+import Sound.TimeLines.OSC
 
 --
 import Control.Concurrent (forkIO, ThreadId)
@@ -28,89 +17,15 @@ import Control.Monad.Reader
 import Control.Monad.State
 
 import Data.Fixed
-import Data.IORef
-
-import Debug.Trace
 
 default (Double)
 
-type Window = (Time, Time)
-
--- A timeline (file written to disk) has a start (> 0) and end point,
--- a samplerate and a string that denotes which parameter of the synth it controls
-data TLinfo = TLinfo {infWindow::Window,
-                      infSR::Int,
-                      infParam::String
-                     }
-              deriving (Eq, Show)
-
-
--- A TimeLine is made up of a signal (defined over infinite time starting from 0)
--- and a TLinfo,
-data TimeLine = TimeLine {tlSig::Signal,
-                          tlInfo::TLinfo
-                         }
-
-
-
---data ParamList = ParamList {plParams :: [String],  }
-
-
---synth :: Synth -> (Time -> ParamList) -> IO ()
---synth sdef f = do
-  
-
-
-
-
-
--- Duration of file to be written and played back
-infDur :: TLinfo -> Time
-infDur (TLinfo (s, e) _ _) = e - s
-
--- The number of frames that will be written to file
-infNumFrames :: TLinfo -> Int
-infNumFrames i = Pr.floor $ infDur i * fromIntegral (infSR i)
-
-
-
-defaultSampleRate = 700
-defaultInfo :: Window -> String -> TLinfo
-defaultInfo w argName = TLinfo w defaultSampleRate argName
-
---Takes a TLinfo and returns the ReadWrite handle to a file
-openHandle :: TLinfo -> IO SF.Handle
-openHandle i = SF.openFile filename SF.ReadWriteMode info
-        where filename = infParam i
-              format = SF.Format SF.HeaderFormatW64 SF.SampleFormatDouble SF.EndianFile
-              info = SF.Info (infNumFrames i) (infSR i) 1 format 1 True
-
-closeHandle :: SF.Handle -> IO()
-closeHandle = SF.hClose
-
-
--- withHandle :: TLinfo -> ()
-
-
--- Render the TimeLine over the duration specified by the TLinfo
-getVals :: TimeLine -> [Value]
-getVals (TimeLine sig info@(TLinfo (s, e) _ _)) = map f domain
-  where f = sigFunc sig
-        domain = fromToIn s e $ infNumFrames info
-
-
---Takes a TimeLine and returns a Pointer to an array of its values
-getArrayPtr :: TimeLine -> IO (Ptr Double)
-getArrayPtr tl = do
-  ptr <- MA.newArray $ getVals tl
-  return ptr
 
 --Takes a TimeLine, writes it to a file, and returns number of frames written
 writeTL :: TimeLine -> IO Int
 writeTL tl@(TimeLine sig info) = do
   let fileName = infParam info
-  --IO.openFile fileName IO.ReadWriteMode
-  _ <- removeIfExists fileName -----------------better way?
+  _ <- removeFileIfExists fileName
   h <- openHandle info
   arrayPtr <- getArrayPtr tl
   framesWritten <- SF.hPutBuf h arrayPtr $ infNumFrames info --infoSR info
@@ -118,45 +33,19 @@ writeTL tl@(TimeLine sig info) = do
   return framesWritten
 
 
-
---keeping track of the time window to render each TimeLine over
-{-# NOINLINE globalWindowRef #-}
-globalWindowRef :: IORef Window
-globalWindowRef = unsafePerformIO $ newIORef (0, 1)
-
-{-# NOINLINE globalUDPRef #-}
-globalUDPRef :: FD.UDP
-globalUDPRef = unsafePerformIO $ OSC.openUDP "127.0.0.1" 57120
-
---Updates the global time Window
-window :: Time -> Time -> IO Window
-window s e = do
-  sendMessage "/TimeLines/window" $ show dur
-  writeIORef globalWindowRef (s, e)
-  return (s, e)
-    where dur = e-s
-  
 --Renders all timelines with the new Window
 --writeAllTimelines = undefined
 --find which parameters are defined, get their signal, render it
 --over the current window, reload buffers
 
 --Render a timeline over the current window and write it to a file
-writeParamFile :: String -> (Time -> Value) -> IO()
+writeParamFile :: Param -> (Time -> Value) -> IO()
 writeParamFile name sig = do
   currentWindow <- readIORef globalWindowRef
   let info = defaultInfo currentWindow name
 --  DIR.createDirectoryIfMissing True getTempDir
   framesWritten <- writeTL $ TimeLine (Signal sig) info
   return ()
-
-getTempDirectory :: IO FilePath
-getTempDirectory = do
-  d <- DIR.getTemporaryDirectory
-  return $ d ++ "/TimeLines/buffers/"
-
-
-
 
 --Send SC a message to load a certain buffer and update the synth
 sendUpdateMsg :: String -> IO()
@@ -175,40 +64,9 @@ sendParam p sig = do
 
 (<><) = sendParam
 
-{-
-synth' :: SynthName -> SynthDef -> StateT Synth IO() -> IO ()
-synth' synthName synthDef params = do
-  put (synthName, synthDef, []::ParamList)
-  evalStateT params
--}  
-
-type SynthID = String
-type Param = String
-type ParamList = [Param]
-
---type Synth = (SynthName, ParamList)
-
-
 --
 synth :: SynthID -> ReaderT String IO ThreadId -> IO ThreadId
 synth synthID params = do
   pathToTemp <- getTempDirectory
-  DIR.createDirectoryIfMissing True pathToTemp
   runReaderT params $ pathToTemp ++ synthID
 
-
-reset :: IO()
-reset = do
-  sendMessage "/TimeLines/reset" ""
-  (s, e) <- readIORef globalWindowRef
-  let dur = e - s
-  sendMessage "/TimeLines/window" (show dur)
-
-sendPlay :: IO ()
-sendPlay = do
-  sendMessage "/TimeLines/play" ""
-
-sendMessage :: String -> String -> IO()
-sendMessage path str = do
-  let m = OSC.Message path [OSC.string str]
-  FD.sendOSC globalUDPRef m
